bc10f68dcacdd681b21a903a9fd7be746e9a4fd9
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-10-22 21:01:06 -0500

    Add files via upload

--- bufferIO.c
+++ bufferIO.c
@@ -1,112 +1,98 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include "bufferIO.h"
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include "bufferIO.h"
 
 
 My_File *open_file(const char *path, int flag)
 {
-    int fd; 
-    My_File *file = malloc(sizeof(My_File));
-    if(flag == W)
+    int fd;
+    My_File *file; 
+    if(flag == R)
     {
-        // Check if file exists and WRITE MODE
-        if(access(path, F_OK) == 0) 
-        {
-            fd = open(path, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) == -1;
-            if(fd == -1)
-            {
-                perror("Error opening file!");
-                free(file);
-                return NULL;
-            }
-            else if( fd != -1) 
-            { 
-                file->fd = fd;
-                file->offset = lseek(fd, 0, SEEK_CUR); 
-                file->flag = flag; 
-                printf("File has been opened successfully in WRITE MODE\n");   
-                return file;
-            }
-        }
-        else
-        {
-            fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) == -1;
-            if(fd == -1)
-            {
-                perror("Error creating file!");
-                free(file);
-                return NULL;
-            }
-            else 
-            { 
-                file->fd = fd;
-                file->offset = lseek(fd, 0, SEEK_CUR);
-                file->flag = flag; 
-                printf("File has been created successfully in WRITE_MODE\n"); 
-                return file; 
-            }
-        }
-    }
-    else if(flag == R)
-    {
-        fd = open(path, O_RDONLY);
+        fd = open(path, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
         if(fd == -1)
         {
-            perror("File does not exist");
-            free(file);
+            perror("Error occured reading file: ");
             return NULL;
-        } 
-        else
-        { 
+        }
+        file = malloc(sizeof(My_File));   /*add malloc check*/
+        file->fd = fd; 
+        file->flag = flag;
+        file->offset = lseek(fd, 0, SEEK_CUR);
+        printf("File has been opened successfully in READ MODE\n");
+        return file;
+    }
+    else if(flag == W)
+    {
+        if(access(path, F_OK) == 0)
+        {
+            fd = open(path, O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if(fd == -1)
+            {
+                perror("Error opening file");
+                return NULL;
+            }
+            file = malloc(sizeof(My_File));   /*add malloc check*/
             file->fd = fd; 
+            file->flag = flag;
             file->offset = lseek(fd, 0, SEEK_CUR);
-            file->flag = flag; 
-            printf("File has been opened successfully\n");
+            printf("File has been opened successfully in WRITING MODE\n");
             return file; 
+        }
+        else
+        {
+            /* when file does not exist */
+            fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if(fd == -1)
+            {
+                perror("Error opening file");
+                return NULL;
+            }
+            file = malloc(sizeof(My_File));   /*add malloc check*/
+            file->fd = fd; 
+            file->flag = flag;
+            file->offset = lseek(fd, 0, SEEK_CUR);
+            printf("File has been created and opened successfully in WRITING MODE\n");
+            return file;
         }
     }
     else if(flag == A)
     {
-        if(access(path, F_OK) == 0) 
+        if(access(path, F_OK) == 0)
         {
-            fd = open(path, O_APPEND | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            fd = open(path, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
             if(fd == -1)
             {
-                perror("Error opening file file!");
-                free(file);
+                perror("Error opening file");
                 return NULL;
             }
-            else
-            {    
-                file->fd = fd;
-                file->flag = flag; 
-                file->offset = lseek(fd, 0, SEEK_END);
-                printf("File has been opened successfully in APPEND MODE\n");
-                return file;  
-            }
+            file = malloc(sizeof(My_File));   /*add malloc check*/
+            file->fd = fd; 
+            file->flag = flag;
+            file->offset = lseek(fd, 0, SEEK_CUR);
+            printf("File has been opened successfully in APPEND MODE\n");
+            return file; 
         }
         else
         {
-            fd = open(path, O_APPEND | O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            /* when file does not exist */
+            fd = open(path, O_RDWR | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
             if(fd == -1)
             {
-                perror("Error creating file!");
-                free(file);
+                perror("Error opening file");
                 return NULL;
             }
-            else 
-            { 
-                file->fd = fd;
-                file->offset = lseek(fd, 0, SEEK_END);
-                file->flag = flag;
-                printf("File has been created successfully in APPEND MODE\n"); 
-                return file; 
-            }
+            file = malloc(sizeof(My_File));   /*add malloc check*/
+            file->fd = fd; 
+            file->flag = flag;
+            file->offset = lseek(fd, 0, SEEK_CUR);
+            printf("File has been created and opened successfully in APPEND MODE\n");
+            return file;
         }
     }
     else 
@@ -114,7 +100,118 @@ My_File *open_file(const char *path, int flag)
         errno = EINVAL; // 22 
         perror("Error occured: ");
         errno = 0;
+        return NULL;
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int close_file(My_File *f)
+{
+    if(f == NULL)
+    {
+        write(STDERR_FILENO, "File object is NULL\n", strlen("File object is NULL\n"));
+        return -1;
+    }
+    if(close(f->fd) == -1)
+    {
+        perror("Error closing file");
+        free(f);
+        return -1;
+    }
+    free(f);
+    return 0; 
+}
+
+static char *init_read_buffer(My_File *file, size_t size)
+{
+    char *buffer = malloc(size);
+    return buffer;
+}
+
+static char *init_write_buffer(My_File *file, size_t size)
+{
+    char *buffer = malloc(size);
+    return buffer; 
+}
+
+static int loadbuffer(My_File *file, int buffer_type)
+{
+    size_t bytes_read; 
+
+    if(buffer_type == R)
+    {
+        bytes_read = read(file->fd, file->reading, BUFFER_SIZE);
+        if(bytes_read == -1)
+        {
+            perror(" 2.) Error occurred with loading buffer\n");
+            return -1; 
+        }
+        printf("loadedBufferReading\n");
+    }
+    else if(buffer_type == W)
+    {
+        bytes_read = read(file->fd, file->writing, BUFFER_SIZE);
+        if(bytes_read == -1)
+        {
+            perror("1.) Error occurrd with loading buffer\n");
+            return -1;
+        }
+        printf("loadedBufferWriting\n");
+    }
+    else if(buffer_type == A)
+    {
+        if((bytes_read = read(file->fd, file->reading, BUFFER_SIZE)) == -1)
+        {
+            perror("Error has occurred with loading buffer\n");
+            return -1;
+        }
+        if((bytes_read = read(file->fd, file->writing, BUFFER_SIZE)) == -1)
+        {
+            perror("Error has occurred with loading buffer\n");
+            return -1;
+        }
+        printf("bufferedWritingAndReadingLoaded\n");
+    }
+    else
+    {
+        errno = EINVAL;  /* 22 */
+        perror("Error has occurred in buffer loader");
+        errno = 0;
+        return -1;
+    }
+    return 0; 
+}
+
+int fgetst(My_File *file, char *dest, size_t size)
+{
+    if(!file)
+    {
+        return -1;
+    }
+    loadbuffer(file, file->flag);
+}
+
+
+
+// currently working on buffer loading, specifically, bytes read variable to correctly check errors, then i will 
+// need to countinue my fgets function
--- bufferIO.h
+++ bufferIO.h
@@ -4,31 +4,73 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 16
+#define BUFFER_SIZE 4096    /* defines buffer size */ 
 
 #define R O_RDONLY
 #define W O_WRONLY
-#define B O_RDWR
 #define A O_APPEND
 
+/*  defines File structure */
 typedef struct
 {
-    char writing_buffer[BUFFER_SIZE];
-    char reading_buffer[BUFFER_SIZE];
-    int fd; 
-    int offset; 
-    int flag; 
-    size_t size; 
+
+    char *writing[BUFFER_SIZE];  /* writing buffer */
+    char *reading[BUFFER_SIZE];  /* reading buffer */
+    int fd;                      /* file's descriptor */
+    int offset;                  /* file's offset */
+    int flag;                    /* file mode */
+    size_t size;                 /* file size*/
+
 } My_File;
 
+
+/*
+        My_File *open_file(const char *path, int flags);
+
+        *path - is the path or name of the file 
+        flag  - one of three file modes that are described below. 
+
+Opens file with one of the following flags: 
+    R - Read Mode
+
+        if file does not exist, function returns NULL pointer
+        and outputs error message
+        if file exists, function opens the file in reading mode 
+        and returns pointer to dynamically allocated file
+    
+    W - Write Mode
+
+        if file does not exist, function will create the file
+        and open it in the writing mode. 
+        if file exists, function will open it in writing 
+        mode 
+
+    A - Append Mode 
+        if file does not exist, function will create the file
+        and open it in the writing and reading mode. 
+        if file exists, function will open it in writing and reading
+        mode 
+*/
 My_File *open_file(const char *path, int flags);
 
 int close_file(My_File *f);
 
 
+static char *init_read_buffer(My_File *file, size_t size);
+static char *init_write_buffer(My_File *file, size_t size);
+
+static int destruct_buffers();
+
+static int loadbuffer(My_File *file, int buffer_type);
+
 int read_file();
 int write_file();
 
+int fgetch();
+int fgetst(My_File *file, char *dest, size_t size);
+
+int fputca();
+int fputst();
 
 
 #endif
