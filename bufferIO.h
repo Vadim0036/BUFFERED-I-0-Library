0ed799ff828bd4bef036d79536bb210e1c0be342
Vadym <107150489+Vadim0036@users.noreply.github.com>
2024-10-29 22:06:16 -0500

    Add files via upload

--- bufferIO.h
+++ bufferIO.h
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 
 
-#define BUFFER_SIZE 3/* defines buffer size  4096 Bytes 4 KB */ 
+#define BUFFER_SIZE 1000/* defines buffer size  4096 Bytes 4 KB */ 
 
 #define R O_RDONLY
 #define W O_WRONLY
@@ -19,6 +19,7 @@ typedef struct
 
     char *writing;  /* writing buffer */
     unsigned int writing_buffer_offset;
+
     char *reading;  /* reading buffer */
     unsigned int reading_buffer_offset;
 
